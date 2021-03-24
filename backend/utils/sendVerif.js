4e603f4289b4046354f84a33dc983459f6701ef7
rahali1997 <ahmedrahali256@gmail.com>
2021-03-24 16:52:44 +0100

    eeeeeee

--- backend/utils/sendVerif.js
+++ backend/utils/sendVerif.js
@@ -1,43 +1,20 @@
 import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
-import { google }  from'googleapis'
 
-// These id's and secrets should come from .env file.
-const CLIENT_ID = '854283613866-h7nq2a68ce81ns4kgvgu3vt6c0n616jq.apps.googleusercontent.com';
-const CLEINT_SECRET = 'gfwphS1zQLWuS6sgMnkCO6aT';
-const REDIRECT_URI = 'https://developers.google.com/oauthplayground';
-const REFRESH_TOKEN = '1//04zV36HRZsIR-CgYIARAAGAQSNwF-L9IrOf3IRemxcmE0tAsXPHAH3fc1t1292r45pXBgiqWzsVlKQXxjWjEAXAYEusNHd_8mTXI';
-
-let oAuth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLEINT_SECRET,
-  REDIRECT_URI
-);
-
-  oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
- 
-  
 
 
 function sendVerif(email, verifNumber) {
-    try {
-    const accessToken = await oAuth2Client.getAccessToken();
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            type: 'OAuth2',
-            user: "ahmedrahali256@gmail.com",
-            pass: "appleone",
-            clientId: CLIENT_ID,
-            clientSecret: CLEINT_SECRET,
-            refreshToken: REFRESH_TOKEN,
-            accessToken: accessToken,
+            user: process.env.EMAIL,
+            pass: process.env.PASSWORD
         }
     })
 
     let mailOptions = {
-        from: 'ahmedrahali256@gmail.com',
+        from: 'ahmedrahali250@gmail.com',
         to: email,
         subject: 'PureBox',
         text: 'bienvenue sur PureBox, votre code de vérification est: ' +verifNumber
@@ -49,10 +26,8 @@ function sendVerif(email, verifNumber) {
         } else {
             console.log('email send failed')
         }
-    }) 
-} catch(err) {
-    console.log(err)
-} }
+    })
+}
 
 
 export default sendVerif;
